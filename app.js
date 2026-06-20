d3ca624534d12478e0cb50ee1439f9e6ee7341e7
gemada <gemadatam@gmail.com>
2026-06-20 21:48:20 +0300

    added auth system with mvc structure

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.get("/", (req, res) => {
-  res.json({
-    message: "Kiliwee Backend API is running 🚀",
-  });
-});
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
